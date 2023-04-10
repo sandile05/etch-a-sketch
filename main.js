37755824d245faf8094e2979a5438719e663beaa
sandile05 <106535890+sandile05@users.noreply.github.com>
2023-04-10 10:25:21 +0200

    changeSize function, mouse event listener

--- main.js
+++ main.js
@@ -2,14 +2,31 @@
 function populateBoard(size) {
   //DOM element to access the board class
   let board = document.querySelector(".board");
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => div.remove());
   board.style.gridTemplateColumns = `repeat(${size}, 1fr)` ;
   board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-  for (let i = 0; i < 256; i++) {
+  let amount = size * size;
+  for (let i = 0; i < amount; i++) {
     let square = document.createElement("div");
-    square.style.backgroundColor = "blue";
+
+    //event listener to leave a trail of where the mouse moved
+    square.addEventListener('mouseover', () => {
+      square.style.backgroundColor = 'black'
+    })
+    square.style.backgroundColor = "white";
     board.insertAdjacentElement("beforeend", square);
   }
 }
 
 populateBoard(16); //function call set the size of the board
+
+//change the size of the board using input text
+function changeSize(input){
+  if(input >= 2 && input <= 100){
+    populateBoard(input)
+  } else{
+    console.log("Invalid input")
+  }
+}
